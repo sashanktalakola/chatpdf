028ddab2ea2103e1f27878d80c76af37ab19d91c
sashank <sashanktalakola2@gmail.com>
2025-01-03 14:21:05 -0500

    integrated RAG chain from langchain hub and chatgpt query completion

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import os
 from dotenv import load_dotenv
+import warnings
+import argparse
 from langchain_openai import OpenAIEmbeddings
 from langchain_astradb import AstraDBVectorStore
+from langchain_openai import OpenAI
+from langchain import hub
+from langchain_core.runnables import RunnablePassthrough
 
 from preprocessing import extract_text_from_pdf, get_text_chunks
+
+
+parser = argparse.ArgumentParser(description="ChatPDF App")
+parser.add_argument("--file", type=str)
+parser.add_argument("--query", type=str)
+args = parser.parse_args()
+
+warnings.filterwarnings("ignore")
 
 load_dotenv()
 
@@ -26,8 +39,27 @@ vstore = AstraDBVectorStore(
 )
 
 
-pdf_path = "test-files/rp.pdf"
+pdf_path = args.file
 text = extract_text_from_pdf(pdf_path)
 text_chunks = get_text_chunks(text)
 
 vstore.add_texts(text_chunks)
+
+retriever = vstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+llm = OpenAI()
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+)
+
+query = args.query
+response = chain.invoke(query)
+
+print(response)
