acf9287cf5e9210d284d6f59b461dcafe683ec68
sashank <sashanktalakola2@gmail.com>
2025-01-04 21:28:40 -0500

    integrated cassandra db vectorstore and openai embeddings

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_astradb import AstraDBVectorStore
+
 from preprocessing import extract_text_from_pdf
 
+
+load_dotenv()
+
+ASTRA_DB_APPLICATION_TOKEN = os.environ["ASTRA_DB_APPLICATION_TOKEN"]
+ASTRA_DB_API_ENDPOINT = os.environ["ASTRA_DB_API_ENDPOINT"]
+ASTRA_DB_KEYSPACE = os.environ["ASTRA_DB_KEYSPACE"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+embedding = OpenAIEmbeddings(
+    model="text-embedding-3-small"
+)
+
+vstore = AstraDBVectorStore(
+    collection_name="file_embeddings",
+    embedding=embedding,
+    token=ASTRA_DB_APPLICATION_TOKEN,
+    api_endpoint=ASTRA_DB_API_ENDPOINT,
+    namespace=ASTRA_DB_KEYSPACE,
+)
 
 st.markdown("# ChatPDF")
 
