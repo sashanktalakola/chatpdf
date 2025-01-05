25d6d60767cfe25ca855291635cfbccafc456187
sashank <sashanktalakola2@gmail.com>
2025-01-04 21:37:40 -0500

    bug fixes

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if uploaded_file is not None:
 
     # file_data = uploaded_file.getvalue()
     file_data = extract_text_from_pdf(uploaded_file)
-    st.write(file_data)
+    # st.write(file_data)
 
 query = st.text_input("Enter Question")
 
